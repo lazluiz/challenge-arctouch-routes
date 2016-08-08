abeff4700b0412ab737863c54981aebd32aaf003
lflazzarin <lf.lazzarin@gmail.com>
2016-08-08 00:55:29 -0300

    Let's start

---
+++ app/src/main/java/io/lazluiz/arcchallenge/MainActivity.java
@@ -0,0 +1,13 @@
+package io.lazluiz.arcchallenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
